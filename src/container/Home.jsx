b8b5350fe08132cf929aac5983456ad1ec57c0b2
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-02-19 10:18:48 -0400

    Correcting Home component

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -39,42 +39,54 @@ const Home = () => {
        })
     }, []);
 
+
+    //Hook para poner el scroll hasta el top de la página
     useEffect(() => {
-       
-    }, [input])
+        scrollRef.current.scrollTo(0,0);
+    }, [])
 
     return (
         //se coloca flex container medium devices empty column, high of screen. Aminación a la medida que el div carga ease out
-        <div className= "flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+        <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
             {/* el display será hidden pero dispositivos medium serán flex, porque se tendrán dos diferentes sidebars, una para moviles y otro la web   */}
             <div className="hidden md:flex h-screen flex-initial">
                 {/*Si el usuario existe, entonces envía el usuario, de lo contrario: false*/}
-                <Sidebar user={user && user} />                
-            </div> 
+                <Sidebar user={user && user} />
+            </div>
             {/*se */}
             <div className="flex md:hidden flex-row">
-                {/*se coloca icono y statefield para la función del menú. El setToggle se manda true indicando la posibilidad de abrirlo y cerrarlo*/ }
-                <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
-                <Link to="/">
-                    <img src={logo} alt="logo" className="w-28" />
-                </Link>
-                {/*Ahora apunta a un bloque dinámico de código. No se sabe el usuario pero se saca del local storage*/ }
-                <Link to={`user-profile/${user?._id}`}>
-                    <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
-                </Link>
-            </div>
-            {/*Se comprueba si el sidebar está toggling. Se prepara para mostrar algo */}
-            {ToggleSidebar && ( 
-                /*tomará 4f del espacio*/
-                <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-                    <div className="absolute w-full flex justify-end items-center p-2">
-                        <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+                <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+                    {/*se coloca icono y statefield para la función del menú. El setToggle se manda true indicando la posibilidad de abrirlo y cerrarlo*/}
+                    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
+                    <Link to="/">
+                        <img src={logo} alt="logo" className="w-28" />
+                    </Link>
+                    {/*Ahora apunta a un bloque dinámico de código. No se sabe el usuario pero se saca del local storage*/}
+                    <Link to={`user-profile/${user?._id}`}>
+                        <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+                    </Link>
+                </div>
+                {/*Se comprueba si el sidebar está toggling. Se prepara para mostrar algo */}
+                {ToggleSidebar && (
+                    /*tomará 4f del espacio*/
+                    <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                        <div className="absolute w-full flex justify-end items-center p-2">
+                            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+                        </div>
+                        {/*Se renderiza el sidebar de nuevo. Sidebar user={} MOVIL*/}
+                        <Sidebar closeToggle={setToggleSidebar} user={user && user} />
                     </div>
-                    {/*Se renderiza el sidebar de nuevo. Sidebar user={} MOVIL*/}
-                    <Sidebar closeToggle={setToggleSidebar} user={user && user} />
-                </div>  
-            )}
-            <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}></div>
+                )}
+            </div>
+
+            <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+                <Routes>
+                    {/*  : indica que será dinámico */}
+                    <Route path="/user-profile/:userId" element={<UserProfile />} />
+                    {/* Se muestra usuario sólo si usuario existe */}
+                    <Route path="/*" element={<Pins user={user && user} />} />
+                </Routes>
+            </div>
         </div>
     )
 }
