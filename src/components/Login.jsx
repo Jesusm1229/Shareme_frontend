12e7a638bcabb70d88d2c06ca6a5d333eabcc4a0
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-02-24 12:46:24 -0400

    Correcting Auth error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,11 +14,11 @@ const Login = () => {
     //se crea el hook navigation
     const navigate = useNavigate();
 
-    const responseGoogle= (response) =>{
-        localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const responseGoogle = (response) =>{
+        localStorage.setItem('user', JSON.stringify(response?.profileObj));
 
         //Datos google auth
-        const { name, googleId, imageUrl } = response.profileObj;
+        const { name, googleId, imageUrl } = response?.profileObj;
         
         const doc = {
             _id: googleId,
@@ -34,8 +34,8 @@ const Login = () => {
                 navigate('/', { replace: true})
 
             })
-
     }
+
     return (
         <div className="flex justify-start items-center flex-col h-screen">
             <div className=" relative w-full h-full">
@@ -48,34 +48,33 @@ const Login = () => {
                     autoPlay
                     className="w-full h-full object-cover"
                 />
+
                 <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
                     <div className="p-5">
                         <img src={logo} width="130px" />
                     </div>
 
-                    <div className="shadow-2x1">
+                    <div className="shadow-2xl">
                         <GoogleLogin
-                            //Se necesita el id de Google authentication
-                            clientId= {process.env.REACT_APP_GOOGLE_API_TOKEN}
+                            clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
                             render={(renderProps) => (
                                 <button
-                                  type="button"
-                                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                                  onClick={renderProps.onClick} //viene de googlelogin package
-                                  disabled={renderProps.disabled}
-                                 >
-                                    <FcGoogle className="mr-4"/>Inicia sesión con Google
+                                    type="button"
+                                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}
+                                >
+                                    <FcGoogle className="mr-4" /> Sign in with google
                                 </button>
                             )}
                             onSuccess={responseGoogle}
                             onFailure={responseGoogle}
-                            cookiePolicy='single_host_origin'
+                            cookiePolicy="single_host_origin"
                         />
                     </div>
                 </div>
             </div>
         </div>
-        );
-    
+    );
 };
 export default Login;
